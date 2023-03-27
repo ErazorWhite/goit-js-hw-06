c0b944686b1bdf66aee581e4b3b764c751606823
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-03-27 15:54:49 +0300

    The second task is completed

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientToAdd = [];
+
+ingredients.forEach((ingredient, index) => {
+  ingredientToAdd.push(document.createElement("li"));
+  ingredientToAdd[index].textContent = ingredient;
+  ingredientToAdd[index].classList = "item";
+});
+
+const list = document.querySelector("#ingredients");
+list.append(...ingredientToAdd);
