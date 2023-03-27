85b47b5776f014793f351611da413477586a4c06
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-03-27 14:35:03 +0300

    The first task is completed

---
+++ js/task-01.js
@@ -0,0 +1,22 @@
+const categoriesHtmlParser = [
+  // { headerName: "testName", itemsCount: 5 }
+];
+// Parser for HTML code. Stores array with two arguments.
+// 1. headerName - name of category. Selector: "li.item > h2"
+// 2. itemsCount - count of li items deep in the #categories. Selector: "li.item > ul".children.length
+
+const categorieHeaders = document.querySelectorAll("li.item > h2");
+
+categorieHeaders.forEach((categorie, index) =>
+  categoriesHtmlParser.push({
+    headerName: categorie.textContent,
+    itemsCount:
+      document.querySelectorAll("li.item > ul")[index].children.length,
+  })
+);
+
+console.log(`Number of categories: ${categoriesHtmlParser.length}`);
+categoriesHtmlParser.forEach((categorieHtmlParsed) => {
+  console.log(`Category: ${categorieHtmlParsed.headerName}`);
+  console.log(`Elements: ${categorieHtmlParsed.itemsCount}`);
+});
