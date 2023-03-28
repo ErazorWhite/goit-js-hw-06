6d22d9f43f2a778b6f72cebb100a23eff17a546d
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-03-29 02:49:13 +0300

    task 9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const btnChangeColor = document.querySelector(".change-color");
+const label = document.querySelector(".color");
+
+function onChangeColor() {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  label.textContent = newColor;
+}
+
+btnChangeColor.addEventListener("click", onChangeColor);
