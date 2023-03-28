91bb3388bbd26da6a6d259b67b9e15eac6ac0e36
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-03-29 02:39:46 +0300

    task 6 refactored

--- js/task-06.js
+++ js/task-06.js
@@ -2,12 +2,14 @@ const input = document.querySelector("#validation-input");
 const inputDataLength = Number(input.getAttribute("data-length"));
 
 function onInputCheck() {
+  const inputClasses = input.classList;
+
   if (input.value.length === inputDataLength) {
-    if (!input.classList.contains("valid")) input.classList.add("valid");
-    if (input.classList.contains("invalid")) input.classList.remove("invalid");
+    if (!inputClasses.contains("valid")) inputClasses.add("valid");
+    if (inputClasses.contains("invalid")) inputClasses.remove("invalid");
   } else {
-    if (!input.classList.contains("invalid")) input.classList.add("invalid");
-    if (input.classList.contains("valid")) input.classList.remove("valid");
+    if (!inputClasses.contains("invalid")) inputClasses.add("invalid");
+    if (inputClasses.contains("valid")) inputClasses.remove("valid");
   }
 }
 
