c953f69b5c6040394a158712323ed42ff43c78e1
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-03-29 02:39:00 +0300

    task 8 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,33 @@
+const form = document.querySelector(".login-form");
 
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (!mail || !password) alert("Все поля должны быть заполнены");
+
+  const formData = {
+    mail,
+    password,
+  };
+
+    console.log("🚀 ~ file: task-08.js:12 ~ onFormSubmit ~ formData:", formData);
+
+    form.reset();
+}
+
+// FormData implementation
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   const formData = new FormData(event.currentTarget);
+//   formData.forEach((value, name) => {
+//     console.log("🚀 ~ file: task-08.js:23 ~ formData.forEach ~ name:", name);
+//     console.log("🚀 ~ file: task-08.js:23 ~ formData.forEach ~ value:", value);
+//   });
+
+//   form.reset();
+// }
+
+form.addEventListener("submit", onFormSubmit);
