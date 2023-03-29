fbe77749e597710b5e33aa2046b9b435a857a849
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-03-29 03:38:44 +0300

    task 10 completed

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,45 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector("input[type=number]");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  const divsToAdd = [];
+  let divSizes = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const divToAdd = document.createElement("div");
+    divToAdd.style.height = divSizes + "px";
+    divToAdd.style.width = divSizes + "px";
+    divToAdd.style.backgroundColor = getRandomHexColor();
+
+    divsToAdd.push(divToAdd);
+
+    divSizes += 10;
+  }
+
+  boxesEl.append(...divsToAdd);
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+}
+
+function onCreate() {
+  if (inputEl.valueAsNumber > 0) {
+    createBoxes(inputEl.valueAsNumber);
+  } else {
+    console.log("Here must be non-zero value");
+  }
+}
+
+function onDestroy() {
+  destroyBoxes();
+}
+
+createBtn.addEventListener("click", onCreate);
+destroyBtn.addEventListener("click", onDestroy);
